ddc33e21e3a76a1d46b2eabaca365ecaa5ed3d83
Jakub Kawa <Jakub.Kawa@webcon.com>
2026-03-02 12:25:36 +0100

    Fixes to ps profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,20 +2,25 @@
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
 # COPY TO $PROFILE location
-$ENV:KUBE_EDITOR='code -w'
+$ENV:KUBE_EDITOR = 'code -w'
 
 # -- Git --
 Set-Alias -Name g -Value "git"
 Set-Alias -Name gforce -Value "git push --force-with-lease"
 
-function glog {git log --oneline --graph --decorate --all}
+function glog { git log --oneline --graph --decorate --all }
 
 Set-Alias -Name d -Value "docker"
 Set-Alias -Name k -Value "kubectl"
 Set-Alias -Name tf -Value "terraform"
 
-kubectl completion powershell | Out-String | Invoke-Expression
+if (Get-Command "kubectl" -ErrorAction SilentlyContinue) {
+  kubectl completion powershell | Out-String | Invoke-Expression
+}
 
-# Install https://ohmyposh.dev/docs/installation/windows
-oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/kali.omp.json" | Invoke-Expression
+if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) {
+  # Install https://ohmyposh.dev/docs/installation/windows
+  oh-my-posh init pwsh | Invoke-Expression
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/kali.omp.json" | Invoke-Expression
+}
+
